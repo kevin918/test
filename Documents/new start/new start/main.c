ba20c87720b0b9e8f389d992dcc70ea13b79ade9
Kai Ren <renkai918@gmail.com>
2013-04-06 23:34:49 -0400

    first commit

---
+++ Documents/new start/new start/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  new start
+//
+//  Created by Kai Ren on 3/23/13.
+//  Copyright (c) 2013 Kai Ren. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
